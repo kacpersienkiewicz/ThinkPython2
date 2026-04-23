3aef55b67decbad1bf43c513a4552a46ee94d2d9
Kacper Sienkiewicz <kacpersienkiewicz@gmail.com>
2026-04-22 23:21:39 -0500

    Added Ch 12 Prompts

---
+++ Ch12/1.py
@@ -0,0 +1,9 @@
+'''
+Exercise 12.1. Write a function called most_frequent that takes a string and prints the let-
+ters in decreasing order of frequency. Find text samples from several different languages and see
+how letter frequency varies between languages. Compare your results with the tables at http:// en. wikipedia. org/ wiki/ Letter_ frequencies .
+Solution: https: // thinkpython.com/ code/ most_ frequent. py .
+'''
+
+def most_frequent():
+    return
---
+++ Ch12/3.py
@@ -0,0 +1,7 @@
+'''
+Exercise 12.3. Two words form a “metathesis pair” if you can transform one into the other by
+swapping two letters; for example, “converse” and “conserve”. Write a program that finds all of
+the metathesis pairs in the dictionary. Hint: don’t test all pairs of words, and don’t test all possible
+swaps. Solution: https: // thinkpython. com/ code/ metathesis. py . Credit: This exercise
+is inspired by an example at http: // puzzlers. org .
+'''
